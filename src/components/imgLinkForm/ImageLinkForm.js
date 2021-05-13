0cb8a400c7d83707d19b992535de5814c61774fa
saurav kumar <ksaurav7616@gmail.com>
2021-05-13 19:33:36 +0530

    minor change

--- src/components/imgLinkForm/ImageLinkForm.js
+++ src/components/imgLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 				{'face detection in images'}
 			</p>
 			<div className='w-60 shadow-2 pa4 br3 center pattern'>
-				<input className='f4 pa2 w-70 center' placeholder='enter url of your image' type='text' onChange={ onInputChange }/>
+				<input className='f4 pa2 w-70 center' placeholder='enter url of your image (jpg only)' type='text' onChange={ onInputChange }/>
 				<button className='w-30 grow f4 link ph3 pv2 dib br2 white bg-light-purple'
 						onClick={ onButtonSubmit } >Detect</button>
 			</div>
